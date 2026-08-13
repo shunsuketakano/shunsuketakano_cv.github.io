0bed556d7c140997a99b870340040324fa7c7d73
shunsuketakano <shunsuke.t-8395@akane.waseda.jp>
2026-08-14 06:25:06 +0900

    Update git_last_modified.rb

--- _plugins/git_last_modified.rb
+++ _plugins/git_last_modified.rb
@@ -1,14 +1,31 @@
-Jekyll::Hooks.register :documents, :pre_render do |document|
-  next unless document.collection
-  next unless document.collection.label == "learn"
-
-  path = document.path
-
+def git_date(path, option)
   result = IO.popen(
-    ["git", "log", "-1", "--format=%cI", "--", path],
+    ["git", "log", option, "-1", "--format=%cI", "--", path],
     err: File::NULL,
     &:read
   ).strip
 
-  document.data["last_modified_at"] = result unless result.empty?
+  result unless result.empty?
+end
+
+# Collection documents
+Jekyll::Hooks.register :documents, :pre_render do |document|
+  path = document.path
+
+  document.data["created_at"] =
+    git_date(path, "--reverse")
+
+  document.data["last_modified_at"] =
+    git_date(path, "")
+end
+
+# Normal pages
+Jekyll::Hooks.register :pages, :pre_render do |page|
+  path = page.path
+
+  page.data["created_at"] =
+    git_date(path, "--reverse")
+
+  page.data["last_modified_at"] =
+    git_date(path, "")
 end
